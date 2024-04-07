5d4e7190173519783648eddb7837b2c14a2335d4
priyanka5555 <priyankasharma080396@gmail.com>
2024-04-06 21:18:44 -0400

    New Resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
           </p>
 
           <div>
-            <a href="/oldResume.pdf" download={fileName} style={{ textDecoration: 'none' }}>
+            <a href="/Resume.pdf" download={fileName} style={{ textDecoration: 'none' }}>
           <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md
           bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
             Download Resume 
