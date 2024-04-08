68a523c478a3288b71f118c7e7d5838a689df179
priyanka5555 <priyankasharma080396@gmail.com>
2024-04-08 09:41:11 -0400

    Resized project images + Added title to projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import weather from "../assets/MyProjects/weather.jpg";
 import QRCode from "../assets/MyProjects/QRCode.jpg";
-import ImageGallery from "../assets/MyProjects/Nature.jpg";
+import ImageGallery from "../assets/MyProjects/image_generator.jpg";
 import axios from 'axios'
 
 const Projects = () => {
@@ -9,12 +9,14 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
+      name: 'Weather App',
       src: weather,
       githubUrl: "https://priyanka5555.github.io/Weather_App/",
       githubRepoUrl: "https://github.com/priyanka5555/Weather_App",
     },
     {
       id: 2,
+      name:' QR Code Geneator',
       src: QRCode,
       githubUrl: "https://priyanka5555.github.io/QR_Code_Generator/",
       githubRepoUrl: "https://github.com/priyanka5555/QR_Code_Generator",
@@ -22,6 +24,7 @@ const Projects = () => {
     },
     {
       id: 3,
+      name: 'Image Search Engine',
       src: ImageGallery,
       githubUrl: "https://priyanka5555.github.io/Imagine_search_engine/",
       githubRepoUrl: "https://github.com/priyanka5555/Imagine_search_engine",
@@ -32,7 +35,7 @@ const Projects = () => {
 
   return (
     <div name="projects"
-      className='py-24 sm:py-32 bg-gradient-to-b from-black to-gray-800 w-full h-fit text-white md:h-fit'>
+      className='py-24 sm:py-32 bg-gradient-to-b from-black to-gray-800 w-full h-fit text-white md:h-fit '>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 
@@ -42,9 +45,10 @@ const Projects = () => {
           <p className='py-6'>I have created a few projects using HTML, CSS and javascript. You can also check out Live Preview and Source Code.</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-          {projects.map(({ id, src, githubUrl,githubRepoUrl}) => (
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 justify-center'>
+          {projects.map(({ id,name, src, githubUrl,githubRepoUrl}) => (
             <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+              <p className='text-3xl text-white mb-4'>{name}</p>
               <img
                 src={src}
                 alt=""
